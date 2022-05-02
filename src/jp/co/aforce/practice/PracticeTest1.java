a9d08d50e22bc66f902e290be85dbe175ff0726f
Terata Yuri <y.terata@a-force.co.jp>
2022-05-02 12:27:40 +0900

    テスト

---
+++ src/jp/co/aforce/practice/PracticeTest1.java
@@ -0,0 +1,33 @@
+package jp.co.aforce.practice;
+
+public class PracticeTest1 {
+	public static void main(String[] args) {
+		//練習問題2
+		System.out.println("Hello World");
+
+		//練習問題3
+		int x = 11;
+		System.out.println(x);
+
+		//練習問題4
+		int y = 13;
+		int z = 17;
+		System.out.println("x=" + y + " " + "y=" + z);
+
+		//練習問題5
+		int total;
+		total = y + z;
+		System.out.println(total);
+
+		//練習問題6
+		int NUMBER = 6;
+		System.out.println(NUMBER);
+
+		//練習問題7
+		int sum;
+		sum = total + NUMBER;
+		System.out.println(sum);
+
+	}
+
+}
